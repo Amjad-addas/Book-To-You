dac2a41157bca7efe48d79cf5c676fb09ef289d3
Amjad-addas <65742033+Amjad-addas@users.noreply.github.com>
2024-08-29 00:20:40 +0300

    initial  commit

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -3,23 +3,34 @@ import style from './footer.module.css'
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import { Link } from 'react-router-dom';
 function Footer() {
+  let hrf=window.location.pathname;
+
   return (
     <div className={style.footer}>
     <div className={style.container}>
         <div className={style.logo}> <h1 className={style.logoName}>mj book</h1></div>
         <div className={style.headerLinks}>
           <ul className={style.linkList}>
-            <li id = 'link'  className={style.link}><link rel='helo' href="#" /> Home</li>
-            <li id = 'link' className={style.link}><link rel='helo' href="#" />Book</li>
-            <li id = 'link' className={style.link}><link rel='helo' href="#" />Bio</li>
-            <li id = 'link' className={style.link}><link rel='helo' href="#" />Contact</li>
+          <li id = 'link'  className={style.link}><Link   className={style.link} 
+            style={hrf=='/'? {color:'aqua'}:{color:'white'}}
+            to='/'> Home</Link></li>
+            <li id = 'link' className={style.link}><Link   className={style.link} 
+            style={hrf=='/book'? {color:'aqua'}:{color:'white'}}
+            to='/book'>  Book</Link></li>
+            <li id = 'link' className={style.link}><Link  className={style.link} to='/bio' 
+            style={hrf=='/bio'? {color:'aqua'}:{color:'white'}}
+            >Bio</Link> </li>
+            <li id = 'link' className={style.link}><Link  className={style.link} to='/contact'
+            style={hrf=='/contact'? {color:'aqua'}:{color:'white'}}
+            >Contact</Link></li>
           </ul>
         </div>
         <ul className={style.aboutSocial}>
-            <svg className={style.midea}><FacebookIcon className={style.icon}/></svg>
-            <svg className={style.midea}><InstagramIcon className={style.icon}/></svg>
-            <svg className={style.midea}><TwitterIcon  className={style.icon}/></svg>
+            <svg className={style.midea}><FacebookIcon style={{fontSize:'40px'}} className={style.icon}/></svg>
+            <svg className={style.midea}><InstagramIcon style={{fontSize:'40px'}} className={style.icon}/></svg>
+            <svg className={style.midea}><TwitterIcon style={{fontSize:'40px'}}  className={style.icon}/></svg>
         </ul>
 
     </div>
--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import ShopIcon from '@mui/icons-material/Shop';
 import style from './header.module.css'
-import { Icon } from '@mui/material';
 import { Link } from 'react-router-dom';
 function Header() {
+
+
+  let hrf=window.location.pathname;
+  
+
   return (
     <div className={style.container}>
         <div className={style.logo}> <h1 className={style.logoName}>mj book</h1></div>
         <div className={style.headerLinks}>
           <ul className={style.linkList}>
-            <li id = 'link'  className={style.link}><Link   className={style.link}    to='/'> Home</Link></li>
-            <li id = 'link' className={style.link}><Link   className={style.link}  to='/book'>  Book</Link></li>
-            <li id = 'link' className={style.link}><Link  className={style.link} to='/bio'  >Bio</Link> </li>
-            <li id = 'link' className={style.link}><Link  className={style.link} to='/contact' >Contact</Link></li>
-          </ul>
+            
+            <li id = 'link'  className={style.link}><Link   className={style.link} 
+            style={hrf=='/'? {color:'aqua'}:{color:'black'}}
+            to='/'> Home</Link></li>
+            <li id = 'link' className={style.link}><Link   className={style.link} 
+            style={hrf=='/book'? {color:'aqua'}:{color:'black'}}
+            to='/book'>  Book</Link></li>
+            <li id = 'link' className={style.link}><Link  className={style.link} to='/bio' 
+            style={hrf=='/bio'? {color:'aqua'}:{color:'black'}}
+            >Bio</Link> </li>
+            <li id = 'link' className={style.link}><Link  className={style.link} to='/contact'
+            style={hrf=='/contact'? {color:'aqua'}:{color:'black'}}
+            >Contact</Link></li>
+                  
+            </ul>
         </div>
         <div className={style.cart}>
            <ShopIcon
@@ -24,6 +38,7 @@ function Header() {
         </div>
     </div>
   )
+
 }
 
 export default Header
